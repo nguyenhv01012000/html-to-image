8e01a47c3b89b5d9a4f4fb702936e31763d98938
Hoàng Nguyên <nguyenhd01012000@gmail.com>
2023-05-11 22:08:54 +0700

    add

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+const puppeteer = require('puppeteer');
+var bodyParser = require('body-parser')
+app.use(bodyParser.text({ type: 'text/html' }))
+
+app.get('/', function (req, res) {
+    (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.setContent( req.body );
+        await page.screenshot({path: 'example.png'});
+      
+        await browser.close();
+      })()
+    res.send(fs.readFileSync('./example.png', 'base64'));
+    });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
