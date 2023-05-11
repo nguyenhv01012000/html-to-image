2685f1b006f8c4e4ec773116dc955e885c6c3191
Hoàng Nguyên <nguyenhd01012000@gmail.com>
2023-05-11 23:53:35 +0700

    add

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ var app = express();
 var fs = require('fs');
 const puppeteer = require('puppeteer');
 var bodyParser = require('body-parser')
-app.use(bodyParser.text({ type: 'text/html' }))
+var multer = require('multer');
+var forms = multer();
+app.use(bodyParser.json());
+app.use(forms.array()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
     (async () => {
         const browser = await puppeteer.launch({headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox']});
         const page = await browser.newPage();
-        await page.setContent( req.body );
+        await page.goto(req.body.url)
         const a = await page.screenshot({ encoding: "base64" });
 
         await browser.close();
