49b0d3d0b310b72acd8695863cb05a44047b197f
Hoàng Nguyên <nguyenhd01012000@gmail.com>
2023-05-11 23:27:32 +0700

    add

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.use(bodyParser.text({ type: 'text/html' }))
 
 app.get('/', function (req, res) {
     (async () => {
-        const browser = await puppeteer.launch({headless: 'new'});
+        const browser = await puppeteer.launch({headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox']});
         const page = await browser.newPage();
         await page.setContent( req.body );
         await page.screenshot({path: 'example.png'});
       
         await browser.close();
-        await res.send(fs.readFileSync('example.png', 'base64'))
+        await res.send(fs.readFileSync('example.png', 'base64'));
       })()
     });
 
