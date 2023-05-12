82afe8b8664560b13823e9bfd8ef79781e3a09f9
Hoàng Nguyên <nguyenhd01012000@gmail.com>
2023-05-12 20:33:53 +0700

    add

--- index.js
+++ index.js
@@ -6,24 +6,27 @@ var bodyParser = require('body-parser')
 var multer = require('multer');
 var forms = multer();
 app.use(bodyParser.json());
-app.use(forms.array()); 
+app.use(forms.array());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', async (req, res) => {
-    try {    
-      const browser = await puppeteer.launch({headless: 'new'});
-      const page = await browser.newPage();
-      await page.goto(req.body.url)
-      let screenshot = await page.screenshot({ encoding: "base64" });
+  try {
+    const browser = await puppeteer.launch({
+      headless: 'new',
+      executablePath:'/usr/bin/google-chrome-stable'
+    });
+    const page = await browser.newPage();
+    await page.goto(req.body.url)
+    let screenshot = await page.screenshot({ encoding: "base64" });
 
-      res.send(screenshot)
-    } catch(e) {
-        // catch errors and send error status
-        console.error(e);
-        res.send(`Something went wrong while running Puppeteer: ${e}`);
-    } finally {
-      await browser.close();
-    }
+    res.send(screenshot)
+  } catch (e) {
+    // catch errors and send error status
+    console.error(e);
+    res.send(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    await browser.close();
+  }
 });
 
 app.listen(3000, function () {
