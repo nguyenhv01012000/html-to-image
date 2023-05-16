d75b23b80f34fd59e3012f5b5fc40fbcb82f680b
Hoàng Nguyên <nguyenhd01012000@gmail.com>
2023-05-17 00:08:21 +0700

    add

--- index.js
+++ index.js
@@ -33,6 +33,31 @@ app.get('/', async (req, res) => {
   }
 });
 
+app.post('/', async (req, res) => {
+  if (req.query.url == null) return "";
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    executablePath: '/usr/bin/google-chrome-stable'
+  });
+
+  try {
+    const page = await browser.newPage();
+    //await page.emulate(iPhone);
+    await page.goto(decodeURI(req.query.url));
+    if (req.query.width != null && req.query.height != null)
+      await page.setViewport({ width: parseInt(req.query.width), height: parseInt(req.query.height) });
+    let screenshot = await page.screenshot({ encoding: "base64", fullPage: true });
+
+    res.send(screenshot);
+  } catch (e) {
+    // catch errors and send error status
+    console.error(e);
+    res.send(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    await browser.close();
+  }
+});
+
 
 
 app.listen(3000, function () {
