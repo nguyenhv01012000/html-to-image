d520f25dcc0fa57dd884e559be7e0dd0b616a4e5
Hoàng Nguyên <nguyenhd01012000@gmail.com>
2023-07-07 12:46:36 +0700

    add

--- shopee.js
+++ shopee.js
@@ -18,7 +18,7 @@ app.post('/upload', fileUpload({ createParentPath: true }), async (req, res) => 
       headless: 'new',
       args: ["--no-sandbox", "--incognito"],
       //userDataDir: '/dev/null',
-      //executablePath: '/usr/bin/google-chrome-stable'
+      executablePath: '/usr/bin/google-chrome-stable'
     });
   } catch (e) {
     // catch errors and send error status
