2fcf07b6f9f07459016b4cce28364c6602973b39
Hoàng Nguyên <nguyenhd01012000@gmail.com>
2023-07-07 11:15:37 +0700

    add

--- shopee.js
+++ shopee.js
@@ -5,7 +5,10 @@ var bodyParser = require('body-parser');
 var textract = require('textract');
 const path = require("path");
 const fileUpload = require("express-fileupload");
-
+var multer = require('multer');
+var forms = multer();
+app.use(bodyParser.json());
+app.use(forms.array());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -31,26 +34,37 @@ app.post('/upload', fileUpload({ createParentPath: true }), async (req, res) => 
   }
 
   const files = req.files
-  let products = [];
-  let users = [];
-  let passwords = [];
+  let addProducts = [];
+  let reportProducts = [];
+  let cookies = [];
+  let search = [];
 
   Object.keys(files).forEach(key => {
     textract.fromBufferWithName(files[key].name, files[key].data, async function (error, text) {
       const myArray = text.split(" ");
       let j = 0;
-      for (let i = 2; i < myArray.length; i = i + 2) {
-        if (myArray[i] == "Link_product") { j = i; break; }
-        if (myArray[i] != '') users.push(myArray[i]);
-        if (myArray[i + 1] != '') passwords.push(myArray[i + 1]);
+      for (let i = 1; i < myArray.length; i++) {
+        if (myArray[i] == "San_pham_report") { j = i; break; }
+        if (myArray[i] != '') cookies.push(handleCookie(myArray[i]));
       }
+
       for (let i = j + 1; i < myArray.length; i++) {
-        if (myArray[i] != '') products.push(myArray[i])
+        if (myArray[i] == "Them_san_pham") { j = i; break; }
+        if (myArray[i] != '') reportProducts.push(myArray[i])
+      }
+
+      for (let i = j + 1; i < myArray.length; i++) {
+        if (myArray[i] == "Tu_khoa_tim_kiem") { j = i; break; }
+        if (myArray[i] != '') addProducts.push(myArray[i])
+      }
+
+      for (let i = j + 1; i < myArray.length; i++) {
+        if (myArray[i] != '') search.push(myArray[i])
       }
 
       try {
-        for (let i in users) {
-          await autoReport(users[i], passwords[i], products, browser);
+        for (let i in cookies) {
+          await autoReport(cookies[i], search, addProducts, reportProducts, browser);
         }
       } catch (e) {
         // catch errors and send error status
@@ -65,25 +79,125 @@ app.post('/upload', fileUpload({ createParentPath: true }), async (req, res) => 
   return res.json({ status: 'Thành công!', message: "" })
 });
 
-async function autoReport(user, password, products, browser) {
+app.post('/report', async (req, res) => {
+  try {
+    browser = await puppeteer.launch({
+      headless: 'new',
+      args: ["--no-sandbox", "--incognito"],
+      //userDataDir: '/dev/null',
+      executablePath: '/usr/bin/google-chrome-stable'
+    });
+  } catch (e) {
+    // catch errors and send error status
+    console.error(e);
+    res.send(`Something went wrong while running: ${e}`);
+  }
+
+  if (browser == null) {
+    return;
+  }
+
+  let addProducts = [];
+  let reportProducts = [];
+  let cookies = [];
+  let search = [];
+
+
+  let myArray = req.body.cookies.split(",,,");
+  for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i] != '') cookies.push(handleCookie(myArray[i]));
+  }
+
+  myArray = req.body.San_pham_report.split(",,,");
+  for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i] != '') reportProducts.push(myArray[i])
+  }
+
+  myArray = req.body.Them_san_pham.split(",,,");
+  for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i] != '') addProducts.push(myArray[i])
+  }
+
+  myArray = req.body.Tu_khoa_tim_kiem.split(",,,");
+  for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i] != '') search.push(myArray[i])
+  }
+
+  try {
+    for (let i in cookies) {
+      await autoReport(cookies[i], search, addProducts, reportProducts, browser);
+    }
+  } catch (e) {
+    // catch errors and send error status
+    console.error(e);
+  } finally {
+    await browser?.close();
+  }
+
+
+  return res.json({ status: 'Thành công!', message: "" });
+});
+
+function handleCookie(cookie) {
+  let fields = cookie.split("shopee.vn=");
+  let result = []
+  for (let i = 1; i < fields.length; i++) {
+    let name = fields[i].substr(0, fields[i].search("="));
+    let value = fields[i].substr(fields[i].search("=") + 1, fields[i].length);
+    if (value[value.length - 1] == '.') value = value.substr(0, value.length - 2);
+    else value = value.substr(0, value.length - 1);
+    result.push({ 'name': name, 'value': value })
+  }
+  return result;
+}
+
+async function autoReport(cookies, search, addProducts, reportProducts, browser) {
   if (browser == null) {
     return;
   }
   let context = await browser.createIncognitoBrowserContext();
   let page = await context.newPage();
+  await page.goto("https://shopee.vn");
+  await page.setCookie(...cookies);
+  await page.cookies("https://shopee.vn");
 
-  await page.goto("https://shopee.vn/buyer/login");
-  await page.keyboard.press('Enter')
-  await page.waitForSelector('input[type="text"]')
-  await page.type('input[type="text"]', user);
-  await page.waitForSelector('input[type="password"]')
-  await page.type('input[type="password"]', password);
-  await page.click('input[type="password"]');
-  await page.keyboard.press('Enter');
-  await page.waitForNavigation();
-  for (let i in products) {
+  // await page.waitForSelector('input[type="text"]')
+  // await page.type('input[type="text"]', user);
+  // await page.waitForSelector('input[type="password"]')
+  // await page.type('input[type="password"]', password);
+  // await page.click('input[type="password"]');
+  // await page.keyboard.press('Enter');
+  //await page.waitForNavigation();
+
+  for (let i in search) {
     try {
-      await page.goto(products[i]);
+      await page.waitForSelector('input[aria-controls="shopee-searchbar-listbox"]')
+      await page.type('input[aria-controls="shopee-searchbar-listbox"]', search[i]);
+      await page.keyboard.press('Enter');
+      await page.waitForSelector('a[class="_6om6sz"]');
+      await page.click('a[class="_6om6sz"]');
+    } catch (e) {
+      // catch errors and send error status
+      console.error("invalid search:" + search[i]);
+      console.error(e);
+    }
+  };
+
+  for (let i in addProducts) {
+    try {
+      await page.goto(addProducts[i]);
+      await page.waitForSelector('button[class="btn btn-tinted btn--l iFo-rx QA-ylc"]');
+      await page.click('button[class="btn btn-tinted btn--l iFo-rx QA-ylc"]');
+    } catch (e) {
+      // catch errors and send error status
+      console.error("invalid URL:" + addProducts[i]);
+      console.error(e);
+    }
+  };
+
+  for (let i in reportProducts) {
+    try {
+      await page.goto(reportProducts[i]);
       await page.waitForSelector('button[class="GyD5JO"]');
       await page.click('button[class="GyD5JO"]');
       const linkHandlers = await page.waitForXPath("//li[contains(text(), 'Hàng giả, hàng nhái')]");
@@ -94,7 +208,7 @@ async function autoReport(user, password, products, browser) {
       await page.waitForXPath("//div[contains(text(), 'Tố cáo thành công')]");
     } catch (e) {
       // catch errors and send error status
-      console.error("invalid URL:" + products[i]);
+      console.error("invalid URL:" + reportProducts[i]);
       console.error(e);
     }
   };
--- test.js
+++ test.js
@@ -1,13 +1,24 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+shopee.vn=__LOCALE__null=VN;
+.shopee.vn=_gcl_au=1.1.226835862.1688167623;
+shopee.vn=csrftoken=XYKHF8BGBdN1j9hgN4ARqAe1j4ssgbMJ;
+.shopee.vn=AMP_TOKEN=%24NOT_FOUND;
+.shopee.vn=_gid=GA1.2.1989305507.1688167628;
+.shopee.vn=SPC_SI=Z6uaZAAAAAAzcVpnQnA2UzTrcAAAAAAAVDUxUFhqM0k=;
+.shopee.vn=SPC_F=xhP2cMr34yr20Qx8ER8Dql1YwkUvE5MK;
+.shopee.vn=REC_T_ID=f411e70f-179d-11ee-992c-9440c93f643c;
+shopee.vn=_QPWSDCXHZQA=2f8cbcd0-e353-4113-8c2d-fefb6bf070fa;
+.shopee.vn=SPC_CLIENTID=eGhQMmNNcjM0eXIyjophbmtxlkxyyiej;
+shopee.vn=SPC_P_V=siWnkjikwtxHwdQpPzg2nRPhB3q6cOVqFheYT5oJB7E0OCBVhNagAZUTACygeqsyENlgfH6fmpHvaMEb9HNOhUicqDgLMQfgHZCOQ4oiaY+yDxXG5eUMZiUCaBFdUE9Ga4PYsCZke0LFAIolyujORB0cS+ajd2ipegRrTKM/3f0=;
+.shopee.vn=SPC_ST=.OEROWjRPVk9KRm04V1hNSLWk7XO4Kfuo7pnpsKcwL+dJJXmjMwY9D0AoSO/W0AbPa1smnZarKG8xd7QyX7CC6E2c8z+4DaUlxR2lbpqYeDnpEkBxEb9YD2Tzhx6UKBtmj5XgOJbZFA++i6JCINNtja+rVNpzgI24eKlvRaZVNgWK/1LEM+YW0Iy8CoIkp2OK9X33Oyk/u74zHcaELbyotQ==;
+.shopee.vn=SPC_U=1026057589;
+.shopee.vn=SPC_R_T_ID=qRtDBqZFey+bJ0BMtfZof8qzUpAi7QXXBaKnFh9sLi6bpYVt/YVLJm5rtr32yBn2uSBb9M1Dylez+lWn0r1dLYuodD+0JEEHY2ualhfBxZ9Lc5KnweybcO14yp3UxptoEY5olEBebRnwMrMkMG0p+OBXIF/LNuDzRjm6LjJ2Ia8=;
+.shopee.vn=SPC_R_T_IV=bHRENFlWQ2tCc3hiRnpCVA==;
+.shopee.vn=SPC_T_ID=qRtDBqZFey+bJ0BMtfZof8qzUpAi7QXXBaKnFh9sLi6bpYVt/YVLJm5rtr32yBn2uSBb9M1Dylez+lWn0r1dLYuodD+0JEEHY2ualhfBxZ9Lc5KnweybcO14yp3UxptoEY5olEBebRnwMrMkMG0p+OBXIF/LNuDzRjm6LjJ2Ia8=;
+.shopee.vn=SPC_T_IV=bHRENFlWQ2tCc3hiRnpCVA==;
+.shopee.vn=_ga=GA1.1.788426484.1688167626;
+shopee.vn=ds=5b88f9b211ed244c6b9a908b072c4ff6;
+.shopee.vn=_ga_M32T05RVZT=GS1.1.1688167625.1.1.1688167685.60.0.0;
+shopee.vn=shopee_webUnique_ccd=Jm3IJ4Hk4sduzINc6iqWmw%3D%3D%7CeGGFXbA57JJMh9Z4B%2BZqCRmxNk5pAJoRt5%2FagDYzE4thBH1l%2FoBOiQKTG1HBXt%2B7UDDs1rzfm6JSRpNRFbd8AceMA74G47%2FqMQ%3D%3D%7CldxBOTnuzDPIdRBO%7C06%7C3;
+.shopee.vn=SPC_EC=R2Q4RENaS3MySGltZGUxaHr8PMnT0imsp4Ll2zsPtjs8xkr7XRUdjBGvdDeMxCp2/26Ue7Oeb5Lcnkgf+VFxSbw7dQ2xqfNQZx8zkJ/qfwt+B7h37MF8seTEb5Gmkak3UhYIhfzUqdIzppmWgP+OC7WidxsajTbKsHpNjjwDTpc=;
 
-export const options = {
-  vus: 8,
-  duration: '180s',
-};
-
-export default function () {
-  http.get('http://14.225.238.137:3000/convert?url=https://apis.ezpics.vn/createImageFromTemplate/?id=1202&width=1754&height=1754');
-  sleep(1);
-}
-
+buidiep5844381|84705696146|Thong2006|ejmuaixnoyzs@exelica.com|,]X@A_.G|shopee.vn=__LOCALE__null=VN;.shopee.vn=_gcl_au=1.1.226835862.1688167623;shopee.vn=csrftoken=XYKHF8BGBdN1j9hgN4ARqAe1j4ssgbMJ;.shopee.vn=AMP_TOKEN=%24NOT_FOUND;.shopee.vn=_gid=GA1.2.1989305507.1688167628;.shopee.vn=SPC_SI=Z6uaZAAAAAAzcVpnQnA2UzTrcAAAAAAAVDUxUFhqM0k=;.shopee.vn=SPC_F=xhP2cMr34yr20Qx8ER8Dql1YwkUvE5MK;.shopee.vn=REC_T_ID=f411e70f-179d-11ee-992c-9440c93f643c;shopee.vn=_QPWSDCXHZQA=2f8cbcd0-e353-4113-8c2d-fefb6bf070fa;.shopee.vn=SPC_CLIENTID=eGhQMmNNcjM0eXIyjophbmtxlkxyyiej;shopee.vn=SPC_P_V=siWnkjikwtxHwdQpPzg2nRPhB3q6cOVqFheYT5oJB7E0OCBVhNagAZUTACygeqsyENlgfH6fmpHvaMEb9HNOhUicqDgLMQfgHZCOQ4oiaY+yDxXG5eUMZiUCaBFdUE9Ga4PYsCZke0LFAIolyujORB0cS+ajd2ipegRrTKM/3f0=;.shopee.vn=SPC_ST=.OEROWjRPVk9KRm04V1hNSLWk7XO4Kfuo7pnpsKcwL+dJJXmjMwY9D0AoSO/W0AbPa1smnZarKG8xd7QyX7CC6E2c8z+4DaUlxR2lbpqYeDnpEkBxEb9YD2Tzhx6UKBtmj5XgOJbZFA++i6JCINNtja+rVNpzgI24eKlvRaZVNgWK/1LEM+YW0Iy8CoIkp2OK9X33Oyk/u74zHcaELbyotQ==;.shopee.vn=SPC_U=1026057589;.shopee.vn=SPC_R_T_ID=qRtDBqZFey+bJ0BMtfZof8qzUpAi7QXXBaKnFh9sLi6bpYVt/YVLJm5rtr32yBn2uSBb9M1Dylez+lWn0r1dLYuodD+0JEEHY2ualhfBxZ9Lc5KnweybcO14yp3UxptoEY5olEBebRnwMrMkMG0p+OBXIF/LNuDzRjm6LjJ2Ia8=;.shopee.vn=SPC_R_T_IV=bHRENFlWQ2tCc3hiRnpCVA==;.shopee.vn=SPC_T_ID=qRtDBqZFey+bJ0BMtfZof8qzUpAi7QXXBaKnFh9sLi6bpYVt/YVLJm5rtr32yBn2uSBb9M1Dylez+lWn0r1dLYuodD+0JEEHY2ualhfBxZ9Lc5KnweybcO14yp3UxptoEY5olEBebRnwMrMkMG0p+OBXIF/LNuDzRjm6LjJ2Ia8=;.shopee.vn=SPC_T_IV=bHRENFlWQ2tCc3hiRnpCVA==;.shopee.vn=_ga=GA1.1.788426484.1688167626;shopee.vn=ds=5b88f9b211ed244c6b9a908b072c4ff6;.shopee.vn=_ga_M32T05RVZT=GS1.1.1688167625.1.1.1688167685.60.0.0;shopee.vn=shopee_webUnique_ccd=Jm3IJ4Hk4sduzINc6iqWmw%3D%3D%7CeGGFXbA57JJMh9Z4B%2BZqCRmxNk5pAJoRt5%2FagDYzE4thBH1l%2FoBOiQKTG1HBXt%2B7UDDs1rzfm6JSRpNRFbd8AceMA74G47%2FqMQ%3D%3D%7CldxBOTnuzDPIdRBO%7C06%7C3;.shopee.vn=SPC_EC=R2Q4RENaS3MySGltZGUxaHr8PMnT0imsp4Ll2zsPtjs8xkr7XRUdjBGvdDeMxCp2/26Ue7Oeb5Lcnkgf+VFxSbw7dQ2xqfNQZx8zkJ/qfwt+B7h37MF8seTEb5Gmkak3UhYIhfzUqdIzppmWgP+OC7WidxsajTbKsHpNjjwDTpc=;
